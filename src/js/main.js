2ddbd813d80920a95d9661fff19e557958ba9db3
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2024-08-21 23:09:49 +0300

    finsh

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,51 @@
 import "../scss/style.scss";
+
+const sidebarNavItems = document.querySelectorAll('.sidebar__nav-item')
+const paginationNavItems = document.querySelectorAll('.customers__pagination-btn')
+const sidebar = document.querySelector('.sidebar')
+const burgerBtn = document.querySelector('.burger-btn')
+
+let sidebarStatus = false
+
+const clearSelectedSidebarStatus = () => {
+  sidebarNavItems.forEach((item) => {
+    item.classList.remove('sidebar__nav-item--selected')
+  })
+}
+
+const clearSelectedPaginationStatus = () => {
+  paginationNavItems.forEach((item) => {
+    item.classList.remove('customers__pagination-btn--selected')
+  })
+}
+
+const openSideBar = () => {
+  sidebarStatus = true
+  burgerBtn.classList.add('burger-btn--open')
+  sidebar.classList.add('sidebar--open')
+}
+
+const closeSidebar = () => {
+  sidebarStatus = false
+  burgerBtn.classList.remove('burger-btn--open')
+  sidebar.classList.remove('sidebar--open')
+}
+
+sidebarNavItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    clearSelectedSidebarStatus()
+    closeSidebar()
+    item.classList.add('sidebar__nav-item--selected')
+  })
+})
+
+paginationNavItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    clearSelectedPaginationStatus()
+    item.classList.add('customers__pagination-btn--selected')
+  })
+})
+
+burgerBtn.addEventListener('click', () => {
+  sidebarStatus ? closeSidebar() : openSideBar()
+})
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import { defineConfig } from "vite";
 import glob from "fast-glob";
 import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import vitePugPlugin from "vite-plugin-pug";
 
 export default defineConfig({
   plugins: [
+    [vitePugPlugin()],
     ViteImageOptimizer({
       png: {
         quality: 86,
@@ -34,7 +36,10 @@ export default defineConfig({
         glob
           .sync(["./*.html", "./pages/**/*.html"])
           .map((file) => [
-            path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
+            path.relative(
+              __dirname,
+              file.slice(0, file.length - path.extname(file).length)
+            ),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
